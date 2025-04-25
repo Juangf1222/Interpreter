159c149d42fe7f085c853bd60169a1e54fd6cda6
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2025-04-25 15:10:48 -0500

    Cree el main

---
+++ Reportes/src/main/java/udmodelos20251/reportes/Reportes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package udmodelos20251.reportes;
+
+/**
+ *
+ * @author SCVA9
+ */
+public class Reportes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
