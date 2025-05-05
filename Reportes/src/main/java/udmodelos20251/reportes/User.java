10cadfc1fd0c940652eda731e6be0e421aaa692b
02NIN20 <lycoronelp@udistrital.edu.co>
2025-05-04 19:33:33 -0500

    Primer aporte Proxy

---
+++ Reportes/src/main/java/udmodelos20251/reportes/User.java
@@ -0,0 +1,22 @@
+
+class User {
+    private String username;
+    private String role;
+
+    public User(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Boolean isAdmin() {
+        return "admin".equalsIgnoreCase(role);
+    }
+}
