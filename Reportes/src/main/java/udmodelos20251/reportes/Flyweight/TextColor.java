be9cbfa9c62c71fc165d2faa49312bf7d071c0f1
SaiLord28 <151407920+SaiLord28@users.noreply.github.com>
2025-05-04 21:56:43 -0500

    Agregando TextColor.java
    Agregando Clase TextColor

---
+++ Reportes/src/main/java/udmodelos20251/reportes/Flyweight/TextColor.java
@@ -0,0 +1,12 @@
+package udmodelos20251.reportes.Flyweight;
+
+public final class TextColor {
+    private final String hexCode;  
+
+    public TextColor(String hexCode) {
+        this.hexCode = hexCode;
+    }
+
+    public String getHexCode() { return hexCode; }
+}
+}
